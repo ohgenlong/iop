f888f4567e5489efcf16d583e4b122f1b62f91c7
ohgenlong <504264639@qq.com>
2015-02-15 21:29:43 +0800

    ff

--- iop/iop_forms.py
+++ iop/iop_forms.py
@@ -5,26 +5,26 @@ from django.contrib.auth.models import User
 class LoginForm(forms.Form):
     username = forms.CharField(  
         required=True,  
-        label=u"用户名",  
-        error_messages={'required': '请输入用户名'},  
+        label=u"Username",  
+        error_messages={'required': 'please input the username'},  
         widget=forms.TextInput(  
             attrs={  
-                'placeholder':u"用户名",  
+                'placeholder':u"Username",  
             }  
         ),  
     )      
     password = forms.CharField(  
         required=True,  
-        label=u"密码",  
-        error_messages={'required': u'请输入密码'},  
+        label=u"password",  
+        error_messages={'required': u'please input the password'},  
         widget=forms.PasswordInput(  
             attrs={  
-                'placeholder':u"密码",  
+                'placeholder':u"password",  
             }
         ),
     )  
-    def clean(self):  
-        if not self.is_valid():  
-            raise forms.ValidationError(u"用户名和密码为必填项")
-        else:  
-            cleaned_data = super(LoginForm, self).clean() 
+    #def clean(self):  
+     #   if not self.is_valid():  
+    #        raise forms.ValidationError(u"Username and password must not null!")
+     #   else:  
+    #        cleaned_data = super(LoginForm, self).clean() 
