8dfa3d8a1b18bc5b173136cdc59fbab3950d3b45
ohgenlong <ohgenlong@126.com>
2015-02-17 16:32:08 +0800

    0217

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from website.models import *
+
+admin.site.register(TaskLog)
+admin.site.register(TaskList)
+admin.site.register(Host)
+admin.site.register(Action)
+admin.site.register(ServiceList)
+admin.site.register(ServerStatus)
+admin.site.register(UserProfile)
+admin.site.register(Group)
 
 # Register your models here.
