a81aa0e7fd0016b3bafc423a574452e9f719629c
ohgen <ohgenlong@126.com>
2015-04-06 22:08:00 +0800

    添加模板管理页面

--- inv/urls.py
+++ inv/urls.py
@@ -3,14 +3,14 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-   # Supplier url
+    # Supplier url
     url(r'^supplier/$',
        'inv.views.supplier_manage', name='supplier_list'),
                        
     url(r'^supplier/(?P<supplier_id>\d+)/$',
         'inv.views.supplier_manage', name='supplier_manage'),
    
-   # Server url                  
+    # Server url                  
     url(r'^server_part/$', 
         'inv.views.server_part_manage',name='server_part_list'),
                         
@@ -19,19 +19,19 @@ urlpatterns = patterns('',
                         
     url(r'^server_part/(?P<part_name>\w+)/(?P<type_id>\d+)/$',
         'inv.views.server_part_manage', name='server_part_manage'),
-#    
-#    # Template url                                    
-#    url(r'^template/$',
-#        'inv.views.template_manage', name='template_list'),
-#                        
-#    url(r'^template/(?P<template_type>\w+)/$',
-#        'inv.views.template_manage', name='template_type_list'),
-#                        
-#    url(r'^template/(?P<template_type>\w+)/(?P<template_id>\d+)/$',
-#        'inv.views.template_manage', name='template_manage'),
-#                        
-#    url(r'^template/(?P<template_type>\w+)/(?P<template_id>\d+)/io/(?P<io_type>\w+)/$',
-#        'inv.views.template_io_manage', name='template_io_manage'),
+    
+    # Template url                                    
+    url(r'^template/$',
+        'inv.views.template_manage', name='template_list'),
+                        
+    url(r'^template/(?P<template_type>\w+)/$',
+        'inv.views.template_manage', name='template_type_list'),
+                        
+    url(r'^template/(?P<template_type>\w+)/(?P<template_id>\d+)/$',
+        'inv.views.template_manage', name='template_manage'),
+                        
+    url(r'^template/(?P<template_type>\w+)/(?P<template_id>\d+)/io/(?P<io_type>\w+)/$',
+        'inv.views.template_io_manage', name='template_io_manage'),
 #    
 #    # Asset url
 #    url(r'^asset/$',
--- inv/views.py
+++ inv/views.py
@@ -108,34 +108,6 @@ server_parts_map = {
 }
 
 
-templates_map = {
-    'server': {
-        'model': ServerAssetTemplate,
-        'form': ServerAssetTemplateForm,
-        'name': u'Server Template',
-        'color': 'bg-green',
-        'icon': 'ion-android-storage',
-        'io_map': {
-            'hds': {
-                'name': u'Disk',
-                'model': ServerTemplateHDS,
-                'form': ServerTemplateHDSForm,
-            },
-            'nics': {
-                'name': u'Nic',
-                'model': ServerTemplateNICS,
-                'form': ServerTemplateNICSForm,
-            },
-        },
-    },
-    'switch': {
-        'model': SwitchAssetTemplate,
-        'form': SwitchAssetTemplateForm,
-        'name': u'Switch Template',
-        'color': 'bg-aqua',
-        'icon': 'ion-radio-waves',
-    }
-}
 
 @login_required
 def server_part_manage(request, part_name=None, type_id=None):
@@ -157,7 +129,7 @@ def server_part_manage(request, part_name=None, type_id=None):
                 'color': server_parts_map[each_part]['color'],
                 'url': reverse('inventory:server_part_type_list', args=(each_part,)),
             })
-        print all_part
+        
         return render(request, 'inv/server_part_list.html', {
                       'page_name': page_name,
                       'all_part': all_part,
@@ -231,6 +203,231 @@ def server_part_manage(request, part_name=None, type_id=None):
                       })
 
 
+"""
+Template Views
+"""
+templates_map = {
+    'server': {
+        'model': ServerAssetTemplate,
+        'form': ServerAssetTemplateForm,
+        'name': u'Server Template',
+        'color': 'bg-green',
+        'icon': 'ion-android-storage',
+        'io_map': {
+            'hds': {
+                'name': u'Disk',
+                'model': ServerTemplateHDS,
+                'form': ServerTemplateHDSForm,
+            },
+            'nics': {
+                'name': u'Nic',
+                'model': ServerTemplateNICS,
+                'form': ServerTemplateNICSForm,
+            },
+        },
+    },
+    'switch': {
+        'model': SwitchAssetTemplate,
+        'form': SwitchAssetTemplateForm,
+        'name': u'Switch Template',
+        'color': 'bg-aqua',
+        'icon': 'ion-radio-waves',
+    }
+}
+
+def _get_templates(template_type, ret_format='list'):
+    """
+    Get templates list
+    """
+    all_template = {'list': [],
+                     'dict': {},
+                     }
+    template_model = templates_map[template_type]['model']
+    for template in template_model.objects.all():
+        template_info = {
+            "id": template.id,
+            "name": template.name,
+            "manufacturer": template.manufacturer,
+            "height": template.height,
+            "suppliers": ",".join([each.name for each in template.suppliers.all()]),
+            "notes": template.notes,
+        }
+        if template_type == 'server':
+            cpu_info = "%s %s * %s" % (
+                template.cpu.get_manufacturer_display(), template.cpu.name, template.cpu_number)
+            total_memory_size = template.memory.size * \
+                template.memory_number / 1024
+            memory_info = "%sG(%sM * %s)" % (total_memory_size,
+                                             template.memory.size, template.memory_number)
+            total_hds = {}
+            total_hds_capacity = 0
+            hds_info = []
+            for each_hd in template.hds.all():
+                total_hds[each_hd.capacity] = total_hds.get(
+                    each_hd.capacity, 0) + 1
+                total_hds_capacity += each_hd.capacity
+            for each_hd_capacity in total_hds:
+                hds_info.append(
+                    "%sG * %s" % (each_hd_capacity, total_hds[each_hd_capacity]))
+            hds_info = "%sG(%s)" % (total_hds_capacity, ", ".join(hds_info))
+            total_nics = {}
+            nics_info = []
+            for each_nic in template.nics.all():
+                total_nics[each_nic.speed] = total_nics.get(
+                    each_nic.speed, 0) + 1
+            for each_nic_speed in total_nics:
+                nics_info.append(
+                    "%sM * %s" %(each_nic_speed, total_nics[each_nic_speed])
+                )
+            nics_info = ", ".join(nics_info)
+            template_info["cpu"] = cpu_info
+            template_info["memory"] = memory_info
+            template_info["hds"] = hds_info
+            template_info["nics"] = nics_info
+
+        all_template['list'].append(template_info)
+        all_template['dict'][template.id] = template_info
+
+    return all_template.get(ret_format)
+
+@login_required
+def template_manage(request, template_type=None, template_id=None):
+    """
+    Manage template
+    """
+    if not template_type:
+        page_name = u'Template Management'
+        template_order = ['server', 'switch']
+        all_type = []
+        for each_type in template_order:
+            type_map = templates_map[each_type]
+            all_type.append({
+                'name': type_map['name'],
+                'number': type_map['model'].objects.count(),
+                'icon': type_map['icon'],
+                'color': type_map['color'],
+                'url': reverse('inventory:template_type_list', args=(each_type,)),
+            })
+        return render(request, 'inv/template_list.html', {
+                      'page_name': page_name,
+                      'all_type': all_type,
+        })
+    try:
+        template_map = templates_map[template_type]
+    except:
+        raise Http404
+    page_name = u'%s' % template_map['name']
+    if template_id:
+        template = get_object_or_404(template_map['model'], pk=template_id)
+        page_name = u'Edit %s' % template_map['name']
+        if request.method == 'POST':
+            form = template_map['form'](request.POST, request.FILES, instance=template)
+            operate = request.POST.get('operate')
+            if form.is_valid():
+                if operate == 'update':
+                    template = form.save()
+                    messages.success(request, u'%s (%s) Update Success!' %
+                                     (template_map['name'], template))
+                    return HttpResponseRedirect(reverse('inventory:template_type_list', args=(template_type,)))
+                elif operate == 'delete':
+                    template.delete()
+                    messages.success(request, u'%s (%s) Delete Success!' %
+                                     (template_map['name'], template))
+                    return HttpResponseRedirect(reverse('inventory:template_type_list', args=(template_type,)))
+                else:
+                    messages.warning(request, u'Dont support %s operation type!' % operate)
+        else:
+            form = template_map['form'](instance=template)
+        return render(request, 'inv/template.html', {
+                  "form": form,
+                  "page_name": page_name,
+                  "template_type": template_type,
+                  "template_id": template_id,
+                  "template_io_map": template_map.get('io_map'),
+                  })
+    else:
+        template = template_map['model']()
+        if request.method == 'POST':
+            form = template_map['form'](request.POST, request.FILES, instance=template)
+            operate = request.POST.get('operate')
+            if form.is_valid():
+                if operate == 'add':
+                    template = form.save()
+                    messages.success(request, u'%s (%s) Add Success!' %
+                                     (template_map['name'], template))
+                    return HttpResponseRedirect(reverse('inventory:template_type_list', args=(template_type,)))
+                elif operate == 'add_and_manage_io':
+                    template = form.save()
+                    messages.success(request, u'%s (%s) Add Success!' %
+                                     (template_map['name'], template))
+                    return HttpResponseRedirect(reverse('inventory:template_manage',
+                                                        args=(template_type, template.id)))
+        else:
+            form = template_map['form'](instance=template)
+        all_template = _get_templates(template_type)
+        return render(request, 'inv/template_type_list.html', {
+                      "form": form,
+                      "page_name": page_name,
+                      "all_template": all_template,
+                      "template_type": template_type,
+                      })
+
+
+
+@login_required
+def template_io_manage(request, template_type, template_id, io_type):
+    """
+    Manage Template I/O device
+    """
+    try:
+        template_map = templates_map[template_type]
+    except:
+        raise Http404
+    template = get_object_or_404(template_map['model'], pk=template_id)
+    template_io_map = template_map.get('io_map')
+    if not template_io_map:
+        raise Http404
+    template_io = template_io_map.get(io_type)
+    if not template_io:
+        raise Http404
+    page_name = template_io['name']
+    io_model = template_io['model']()
+    if request.method == 'POST':
+        operate = request.POST.get('operate')
+        if operate == 'add':
+            form = template_io['form'](request.POST, instance=io_model)
+            if form.is_valid():
+                io_dev = form.save(commit=False)
+                io_dev.server = template
+                io_dev.save()
+                messages.success(request, u"%s Template (%s) IO_device (%s) Add Success!" %
+                                 (template_map['name'], template, template_io['name']))
+                return HttpResponseRedirect(reverse('inventory:template_manage',
+                                                    args=(template_type, template_id)))
+        elif operate == 'delete':
+            io_id = request.POST.get('io_id')
+            if io_id:
+                io_dev = get_object_or_404(template_io['model'], pk=io_id)
+                io_dev.delete()
+                messages.success(request, u"%s Template (%s) IO_device (%s) Delete Success!" %
+                                 (template_map['name'], template, io_dev)
+                                 )
+            return HttpResponseRedirect(reverse('inventory:template_manage',
+                                                args=(template_type, template_id)))
+        else:
+            messages.warning(request, u'Dont support %s operation type!' % operate)
+    else:
+        form = template_io['form'](instance=io_model)
+    all_io = template_io['model'].objects.filter(server=template).order_by('id')
+    
+    return render(request, 'inv/template_io.html', {
+                  "form": form,
+                  "template_type": template_type,
+                  "template_id": template_id,
+                  "io_type": io_type,
+                  "all_io": all_io,
+                  "page_name": page_name,
+                  })
 
 
 
